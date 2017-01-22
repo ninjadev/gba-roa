00ea0026791919ef1c064e3becbaeaf9fa3036af
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2017-01-22 21:40:33 +0100

    Add variants 3 and 4 of thumb

--- src/parser.rs
+++ src/parser.rs
@@ -15,6 +15,18 @@ enum Instruction {
         operand: u8,
         source_register: u8,
         destination_register: u8,
+    },
+    THUMB3 {
+        tag: u8,
+        opcode: u8,
+        destination_register: u8,
+        unsigned_immediate: u8,
+    },
+    THUMB4 {
+        tag: u8,
+        opcode: u8,
+        source_register: u8,
+        destination_register: u8,
     }
 }
 
@@ -56,11 +68,47 @@ named!(parse_thumb2<Instruction>,
     )
 );
 
+named!(parse_thumb3<Instruction>,
+    bits!(
+        do_parse!(
+            tag: tag_bits!(u8, 3, 0b001) >>
+            opcode: take_bits!(u8, 2) >>
+            destination_register: take_bits!(u8, 3) >>
+            unsigned_immediate: take_bits!(u8, 8) >>
+            (Instruction::THUMB3 {
+                tag: tag,
+                opcode: opcode,
+                destination_register: destination_register,
+                unsigned_immediate: unsigned_immediate,
+            })
+        )
+    )
+);
+
+named!(parse_thumb4<Instruction>,
+    bits!(
+        do_parse!(
+            tag: tag_bits!(u8, 6, 0b010000) >>
+            opcode: take_bits!(u8, 4) >>
+            source_register: take_bits!(u8, 3) >>
+            destination_register: take_bits!(u8, 3) >>
+            (Instruction::THUMB4 {
+                tag: tag,
+                opcode: opcode,
+                source_register: source_register,
+                destination_register: destination_register,
+            })
+        )
+    )
+);
+
 named!(parse_thumb<Vec<Instruction>>,
     many0!(
         alt!(
             parse_thumb1 |
-            parse_thumb2
+            parse_thumb2 |
+            parse_thumb3 |
+            parse_thumb4
         )
     )
 );
@@ -73,6 +121,8 @@ mod tests {
     fn parse_instruction() {
         let move_shift_thumb1 = vec![ 0b00011101, 0b11001000, ];
         let add_immediate_thumb2 = vec![ 0b00011101, 0b11001000, ];
-        println!("{:?}", parse_thumb(&move_shift_thumb1));
+        let add_immediate_128_to_r7 = vec![ 0b00110111, 0b10000000 ];
+        let rotate_r4_right_by_r5 = vec![ 0b01000001, 0b11101100 ];
+        println!("{:?}", parse_thumb(&rotate_r4_right_by_r5));
     }
 }
